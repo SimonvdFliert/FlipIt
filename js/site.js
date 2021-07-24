dea8f7047a6eeed49bb0bac516033b917c3ac9c4
SimonvdFliert <Simonvdfliert@hotmail.com>
2021-07-24 16:17:16 +0200

    Made the program working, need to finish layout

--- js/site.js
+++ js/site.js
@@ -1,4 +1,36 @@
-// Call Hello World
-function helloWorld(){
-    alert("hello World");
+// Get string from the page
+function getString(){
+    
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+    
+    let revString = reverseString(userString);
+    
+    displayString(revString);
+
+}
+
+// Reverse the string
+function reverseString(userString){
+
+    let revString = [];
+
+    // reverse a string using a for loop
+    for(let index = userString.length - 1; index >= 0; index--){
+        revString += userString[index];
+    }
+
+    return revString;
+}
+
+// Display the reversed string to the user
+function displayString(revString){
+
+    // write the message to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    // show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+
 }
